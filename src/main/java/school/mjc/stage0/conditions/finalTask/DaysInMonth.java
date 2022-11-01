da2354e8026e3a8935266b79a6b9786b8f2db2c3
Khusniddinova <2110146@newuzbekistanuniversity.uz>
2022-11-01 19:15:58 +0500

    Final

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,43 +2,51 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-        if (year%4!=0 && year%100==0 && year%1000!=0){
-            switch (month){
-                case 1:
-                case 3:
-                case 5:
-                case 7:
-                case 8:
-                case 10:
-                case 12:
-                    System.out.println(31); break;
-                case 2:
-                    System.out.println(28); break;
-                case 4:
-                case 6:
-                case 9:
-                case 11:
-                    System.out.println(30); break;
+        if (year > 0 && 0<month && month<13) {
+            if (year % 4 != 0 && year % 100 == 0 && year % 1000 != 0) {
+                switch (month) {
+                    case 1:
+                    case 3:
+                    case 5:
+                    case 7:
+                    case 8:
+                    case 10:
+                    case 12:
+                        System.out.println(31);
+                        break;
+                    case 2:
+                        System.out.println(28);
+                        break;
+                    case 4:
+                    case 6:
+                    case 9:
+                    case 11:
+                        System.out.println(30);
+                        break;
+                }
+            } else {
+                switch (month) {
+                    case 1:
+                    case 3:
+                    case 5:
+                    case 7:
+                    case 8:
+                    case 10:
+                    case 12:
+                        System.out.println(31);
+                        break;
+                    case 2:
+                        System.out.println(29);
+                        break;
+                    case 4:
+                    case 6:
+                    case 9:
+                    case 11:
+                        System.out.println(30);
+                        break;
+                }
             }
         }
-        else {
-            switch (month){
-                case 1:
-                case 3:
-                case 5:
-                case 7:
-                case 8:
-                case 10:
-                case 12:
-                    System.out.println(31); break;
-                case 2:
-                    System.out.println(29); break;
-                case 4:
-                case 6:
-                case 9:
-                case 11:
-                    System.out.println(30); break;
-            }
-        }
+        else System.out.println("invalid date");
     }
 }
