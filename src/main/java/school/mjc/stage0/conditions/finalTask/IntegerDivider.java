e1a1c971bff530a95b41beba77452fe397799038
Khusniddinova <2110146@newuzbekistanuniversity.uz>
2022-11-01 19:05:26 +0500

    Final

--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        int division = dividend/divider;
+        if (division*divider==dividend){
+            System.out.println("can be divided completely");
+        }
+        else
+            System.out.println("cannot be divided completely");
     }
 }
